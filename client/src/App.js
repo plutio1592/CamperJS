3c983934675763b2aeb2e3436d16698f90703556
ldg <code920309@gmail.com>
2022-09-27 13:45:10 +0900

    무한스크롤 추가

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,37 @@ import Mainpage from "./pages/mainpage/Mainpage";
 import Detailpage2 from "./pages/detailpage/Detailpage2";
 import axios from "axios";
 
-function ContentId() {
+// function ContentId() {
+
+//   if (loading) return <div>로딩중..</div>;
+//   if (error) return <div>에러가 발생했습니다</div>;
+
+//   if (!contentId) return null;
+//   console.log(contentId);
+//   return (
+//     <>
+//       <ul>
+//         {contentId.map((contentId) => (
+//           <li key={contentId.contentId}>{contentId.campingName}</li>
+//         ))}
+//       </ul>
+//       <button onClick={fetchContentId}>다시 불러오기</button>
+//     </>
+//   );
+// }
+
+axios
+  .get("http://localhost:4002/camping")
+  .catch(function (error) {})
+  .then((response) => {
+    // console.log(response.data);
+  });
+
+function App() {
   const [contentId, setContentId] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [contentIdImg, setContentIdImg] = useState(null);
 
   const fetchContentId = async () => {
     try {
@@ -24,41 +51,31 @@ function ContentId() {
     setLoading(false);
   };
 
+  const fetchContentIdImg = async () => {
+    try {
+      setError(null);
+      setContentIdImg(contentIdImg);
+      setLoading(true);
+      const responseImg = await axios.get("http://localhost:4002/imageurl");
+      setContentIdImg(responseImg.data);
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
+
   useEffect(() => {
     fetchContentId();
+    fetchContentIdImg();
   }, []);
 
-  if (loading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다</div>;
-
-  if (!contentId) return null;
-  console.log(contentId);
-  return (
-    <>
-      <ul>
-        {contentId.map((contentId) => (
-          <li key={contentId.contentId}>{contentId.campingName}</li>
-        ))}
-      </ul>
-      <button onClick={fetchContentId}>다시 불러오기</button>
-    </>
-  );
-}
-
-axios
-  .get("http://localhost:4002/camping")
-  .catch(function (error) {})
-  .then((response) => {
-    // console.log(response.data);
-  });
-
-function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Mainpage />} />
+        <Route path="/" element={<Mainpage 
+          contentId = {contentId}
+          contentIdImg = {contentIdImg}/>} />
         {/* <Route path="/detailpage" element={<Detailpage />} /> */}
-        <Route path="/ContentId" element={<ContentId />} />
         <Route path="/detailpage2" element={<Detailpage2 />} />
       </Routes>
     </BrowserRouter>
