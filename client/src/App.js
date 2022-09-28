8f762cd9cdf0eed4578eb18f0deadbd89ca46490
plutio1592 <luke159295@gmail.com>
2022-09-28 16:01:42 +0900

    수정중

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { SignUpModal } from './component/modal/SignUpModal';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div className='box'>
+          <div classNmae='sub_title'>SignUpModal</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
