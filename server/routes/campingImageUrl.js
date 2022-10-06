dade9147d51dbb77c4ed106954ef5ac5f0b5d495
ProjectBYH <wnrhd45@gmail.com>
2022-10-06 16:24:46 +0900

    HTTPS: 작동성공 및 반쪽짜리 회원탈퇴

--- server/routes/campingImageUrl.js
+++ server/routes/campingImageUrl.js
@@ -1,36 +1,38 @@
-const express = require('express')
-// const sequelize = require('sequelize');
-// const {campingImageURL, camping} = require('../models')
-dbConfig = require('../config/database');
-const conn = dbConfig.init();
-dbConfig.connect(conn);
+////주석형태로 백업
 
-const router = express.Router()
+// const express = require('express')
+// // const sequelize = require('sequelize');
+// // const {campingImageURL, camping} = require('../models')
+// dbConfig = require('../config/database');
+// const conn = dbConfig.init();
+// dbConfig.connect(conn);
 
-router.route('/')
-  .get(async (req, res, next) => {
-    try {
-      const imageUrl = "SELECT contentId, group_concat(imageUrl) FROM camper_db.campingimageurls group by contentId"
-      conn.query(imageUrl, function (err, result, fields) {
-        if (err) throw err
-        res.json(result)
-      })
-    }
-    catch (err) {
-      console.error(err)
-      next(err)
-    }
-  })
+// const router = express.Router()
 
-// router.get('/', (req, res) => {
-//   const imageUrl = "SELECT contentId, group_concat(imageUrl) FROM camper_db.campingimageurls group by contentId"
-//   conn.query(imageUrl, function (err, result, fields) {
-//     if (err) throw err
-//     res.json(result)
+// router.route('/')
+//   .get(async (req, res, next) => {
+//     try {
+//       const imageUrl = "SELECT contentId, group_concat(imageUrl) FROM camper_db.campingimageurls group by contentId"
+//       conn.query(imageUrl, function (err, result, fields) {
+//         if (err) throw err
+//         res.json(result)
+//       })
+//     }
+//     catch (err) {
+//       console.error(err)
+//       next(err)
+//     }
 //   })
-// })
 
-module.exports = router
+// // router.get('/', (req, res) => {
+// //   const imageUrl = "SELECT contentId, group_concat(imageUrl) FROM camper_db.campingimageurls group by contentId"
+// //   conn.query(imageUrl, function (err, result, fields) {
+// //     if (err) throw err
+// //     res.json(result)
+// //   })
+// // })
+
+// module.exports = router
 
 
 
